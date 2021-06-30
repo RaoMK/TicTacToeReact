cdb09bb2d896fe292eb5a3b8ce73f3238c7d833d
raomk <mowglijuddi@gmail.com>
2021-06-30 23:34:57 +0530

    tic tac app push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Icons from "./components/Icons";
 
-function App() {
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+
+  const checkIsWinner = () => {
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5]
+    ) {
+      setWinMessage(`${itemArray[3]} wins`);
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7]
+    ) {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    } else if (
+      //checking for match tie
+      itemArray[0] !== "empty" &&
+      itemArray[1] !== "empty" &&
+      itemArray[2] !== "empty" &&
+      itemArray[3] !== "empty" &&
+      itemArray[4] !== "empty" &&
+      itemArray[5] !== "empty" &&
+      itemArray[6] !== "empty" &&
+      itemArray[7] !== "empty" &&
+      itemArray[8] !== "empty"
+    ) {
+      setWinMessage("Match Tie");
+    }
+  };
+
+  const changeItem = (itemNumber) => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "cross" : "smile";
+      setIsCross(!isCross);
+    } else {
+      return toast("Already Filled", { type: "error" });
+    }
+
+    checkIsWinner();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-screen h-screen bg-gray-800 text-white items-center p-10">
+      <ToastContainer position="bottom-center" />
+      <h1 className="text-4xl">Tic Tac Game Build with ReactJS & Tailwind</h1>
+      {winMessage ? (
+        <div className="mt-4 mb-4 ">
+          <h1 className="text-center text-2xl capitalize font-bold text-green-600">
+            {winMessage}
+          </h1>
+          <button
+            className="bg-blue-800 p-3 my-4 rounded-md font-bold"
+            onClick={reloadGame}
+          >
+            Restart Game
+          </button>
+        </div>
+      ) : (
+        <h1 className="text-center text-red-500 text-xl mt-5">
+          {isCross ? "Cross's" : "Smile's"} Turn
+        </h1>
+      )}
+      <div className="grid grid-cols-3 grid-rows-3 mt-10 gap-5 ">
+        {itemArray.map((item, index) => (
+          <div
+            onClick={() => changeItem(index)}
+            className=" cursor-pointer flex h-20 w-20 bg-yellow-500 rounded-md justify-center items-center"
+          >
+            <Icons name={item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Icons.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Icons = ({ name }) => {
+  switch (name) {
+    case "smile":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      );
+
+    case "cross":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      );
+
+    default:
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+      );
+  }
+};
+
+export default Icons;
